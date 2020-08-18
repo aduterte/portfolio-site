ff4de5cf3e1fe7e109d3544e858577c6a2bc9d7d
aduterte <alexduterte@gmail.com>
2020-08-18 14:38:27 -0400

    setting up project cards

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -4,6 +4,14 @@ export default function Projects(){
     return(
         <div className="projects-container">
             <h2>Technical Projects</h2>
+            <div className="project-list">
+                <div className="project-card">
+
+                </div>
+                <div className="project-card">
+
+                </div>
+            </div>
         </div>
     )
 }
